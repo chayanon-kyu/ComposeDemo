d5b40dd230004c077f300064d38d3149d0e2bde9
Chayanon Ardkham <develop@chayanon.dev>
2024-09-20 09:46:39 +0700

    Initial Commit

---
+++ composeApp/src/commonMain/kotlin/compose/project/demo/App.kt
@@ -0,0 +1,98 @@
+package compose.project.demo
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import composedemo.composeapp.generated.resources.Res
+import composedemo.composeapp.generated.resources.compose_multiplatform
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            // DeliveryDetail
+            Row {
+                TextButton(
+                    onClick = {},
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .border(
+                                border = ButtonDefaults.outlinedBorder,
+                                shape = RoundedCornerShape(4.dp)
+                            )
+                            .padding(8.dp),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Column(
+                            verticalArrangement = Arrangement.Top
+                        ) {
+                            Text("🚛") // Change To Image
+                        }
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(4.dp)
+                        ) {
+                            Text(
+                                "centralwOrld 999/9 Rama I Rd, Pathum Wan, Bangkok 10330",
+                                style = TextStyle(
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color.DarkGray
+                                ),
+                                maxLines = 2
+                            )
+                            Text(
+                                "No note to the driver",
+                                style = TextStyle(
+                                    fontSize = 12.sp,
+                                    fontWeight = FontWeight.Normal,
+                                    color = Color.LightGray
+                                )
+                            )
+                        }
+                        Column(
+                            verticalArrangement = Arrangement.Top
+                        ) {
+                            Text(">") // Change To Image
+                        }
+                    }
+                }
+            }
+
+            // Slot
+            Spacer(modifier = Modifier.fillMaxHeight())
+        }
+    }
+}
