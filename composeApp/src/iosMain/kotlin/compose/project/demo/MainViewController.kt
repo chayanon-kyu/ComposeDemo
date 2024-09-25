be1e9a84eae1098623daa2232351bd3146d2033a
Chayanon Ardkham <develop@chayanon.dev>
2024-09-25 14:02:26 +0700

    Connect with ComposeDemoIOS

--- composeApp/src/iosMain/kotlin/compose/project/demo/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/compose/project/demo/MainViewController.kt
@@ -1,5 +1,9 @@
 package compose.project.demo
 
 import androidx.compose.ui.window.ComposeUIViewController
+import platform.UIKit.UIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController(): UIViewController =
+    ComposeUIViewController {
+        CartScene()
+    }
